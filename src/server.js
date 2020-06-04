5790a29de150474da151fd8dc8c01dd408c6d70a
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-06-04 12:15:11 -0300

    chore: Using template engine

--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
-
 const server = express();
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
 
 server.use(express.static('public'))
 
+
 server.get('/', function(req, res){
-    res.sendFile(__dirname + '/views/index.html')
+    return res.render('index.html')
 })
 
 server.get('/create-point', function(req, res){
-    res.sendFile(__dirname + '/views/create-point.html')
+    return res.render('create-point.html')
 })
+
+server.get('/search', function(req, res){
+    return res.render('search-results.html')
+})
+
 
 server.listen(3000, (req, res) => {
     console.log('Server is running...')
